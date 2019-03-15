ea545edf01f525ada6c9e161d3cfd6579a9fe599
nihel <nihel.zerria@gmail.com>
2019-03-15 11:17:37 +0100

    user 1 add something on appDelegate file

--- applicationtest/AppDelegate.swift
+++ applicationtest/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("user 1 add something 1 on this file")
         return true
     }
 
